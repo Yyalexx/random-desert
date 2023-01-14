6f907d9b0e959332f6d3e1daaa3cd213f1f00066
Semyon Sorokin <57331385+SimonLen@users.noreply.github.com>
2023-01-14 14:37:10 +0300

    Removed unused function

--- web-application on flask/app.py
+++ web-application on flask/app.py
@@ -15,9 +15,5 @@ def about():
     return render_template('about.html')
 
 
-@app.route('/capitalize/<word>/')
-def capitalize(word):
-    return '<h1>{}</h1>'.format(escape(word.capitalize()))
-
 if __name__ == "__main__":
     app.run(debug=True)
