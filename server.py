08b48f259a0b7e960ed3c50562febf5df65cccdf
mariesafin <72822226+mariesafin@users.noreply.github.com>
2023-01-14 18:09:32 +0300

    Add files via upload
    Добавлен поиск в файле с данными по наименованию блюда

--- server.py
+++ server.py
@@ -1,22 +1,40 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
+import json
+import logging
+
 
 app = Flask(__name__)
 
+logging.basicConfig(level=logging.DEBUG)
+
 @app.route('/')
 def hello():
-    return 'Аюна громко пукает'
+    return render_template('index.html')
 
 #Обрабочик запросов
-#Тут надо добавить, чтобы инфу он брал из наших файлов с данными или БД
 @app.route('/api/add_message', methods=['GET', 'POST'])
 def add_message():
     # получаем JSON со страницы
     content = request.json 
-    # print(content['food'])
-    
+    #тут у нас есть JSON с наименованием блюда
+        
     # отдаем JSON с рецептом
-    return jsonify({"recipe":'SVEKLU V KASTRULU KIN EPTA I VOT TEBE ' + content['food']})
+    return jsonify(save_recipe(content['food']))
 
+
+
+def save_recipe(name):
+    with open("5_recipes.json") as recipes:  # ссылка на JSON файл с рецептами
+        data = json.load(recipes)
+
+        for key in data.keys():
+            if data[key]["recipe_name"]==name:  # находим запрашиваемый рецепт в JSON файле
+                recipe = data[key]
+                
+                return recipe
+        else:        
+            return {"recipe":'SVEKLU V KASTRULU KIN EPTA I VOT TEBE ' + name}
+    
 
 if __name__ == '__main__':
     app.run(port=5000,debug=True)
