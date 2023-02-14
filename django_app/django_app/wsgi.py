2cc6a1f8ddac8f3179b91c3c002d82f52be48681
Semyon Sorokin <57331385+SimonLen@users.noreply.github.com>
2023-02-14 20:23:48 +0300

    wsgi exceptions handling added

--- django_app/django_app/wsgi.py
+++ django_app/django_app/wsgi.py
@@ -8,9 +8,21 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
-
+import time
+import traceback
+import signal
+import sys
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_app.settings')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'django_app.settings'
 
-application = get_wsgi_application()
+try:
+    application = get_wsgi_application()
+    print('WSGI without exception')
+except Exception:
+    print('handling WSGI exception')
+    # Error loading applications
+    if 'mod_wsgi' in sys.modules:
+        traceback.print_exc()
+        os.kill(os.getpid(), signal.SIGINT)
+        time.sleep(2.5)
