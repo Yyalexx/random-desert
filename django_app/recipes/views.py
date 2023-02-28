efcfa85580fad869776a078ff40d5b11bd6d1178
SemyonSorokin <simonlen.gba@gmail.com>
2023-03-01 00:01:02 +0300

    Filter bag fixed

--- django_app/recipes/views.py
+++ django_app/recipes/views.py
@@ -32,6 +32,7 @@ class HomeView(TemplateView):
 
 class SearchView(ListView):
     model = Recipe
+    ordering = 'name_recipe'
     template_name = 'recipes.html'
     context_object_name = 'recipes'
     paginate_by = 10
@@ -42,39 +43,89 @@ class SearchView(ListView):
         meal_types = self.request.GET.getlist('meal_type')
         ingredients = self.request.GET.getlist('ingredients')
 
-        if len(recipe) > 0 and len(cuisines) > 0 and len(meal_types) > 0:
+        if len(recipe) > 0 and len(cuisines) > 0 and len(meal_types) > 0 and len(ingredients) > 0:
+            queryset = Recipe.objects.filter(
+                Q(name_recipe__icontains=recipe) &
+                Q(cuisine__in=cuisines) &
+                Q(meal_type__in=meal_types) &
+                Q(connections__id_ingr__group_name_ingr__in=ingredients)
+            )
+            return queryset
+        elif len(recipe) == 0 and len(cuisines) > 0 and len(meal_types) > 0 and len(ingredients) > 0:
+            queryset = Recipe.objects.filter(
+                Q(cuisine__in=cuisines) &
+                Q(meal_type__in=meal_types) &
+                Q(connections__id_ingr__group_name_ingr__in=ingredients)
+            )
+            return queryset
+        elif len(recipe) > 0 and len(cuisines) == 0 and len(meal_types) > 0 and len(ingredients) > 0:
+            queryset = Recipe.objects.filter(
+                Q(name_recipe__icontains=recipe) &
+                Q(meal_type__in=meal_types) &
+                Q(connections__id_ingr__group_name_ingr__in=ingredients)
+            )
+            return queryset
+        elif len(recipe) > 0 and len(cuisines) > 0 and len(meal_types) == 0 and len(ingredients) > 0:
+            queryset = Recipe.objects.filter(
+                Q(name_recipe__icontains=recipe) &
+                Q(cuisine__in=cuisines) &
+                Q(connections__id_ingr__group_name_ingr__in=ingredients)
+            )
+            return queryset
+        elif len(recipe) > 0 and len(cuisines) > 0 and len(meal_types) > 0 and len(ingredients) == 0:
             queryset = Recipe.objects.filter(
                 Q(name_recipe__icontains=recipe) &
                 Q(cuisine__in=cuisines) &
                 Q(meal_type__in=meal_types)
             )
             return queryset
-        elif len(recipe) == 0 and len(cuisines) > 0 and len(meal_types) > 0:
-            queryset = Recipe.objects.filter(
-                Q(cuisine__in=cuisines) &
-                Q(meal_type__in=meal_types)
-            )
-            return queryset
-        elif len(recipe) > 0 and len(cuisines) == 0 and len(meal_types) > 0:
-            queryset = Recipe.objects.filter(
-                Q(name_recipe__icontains=recipe) &
-                Q(meal_type__in=meal_types)
-            )
-            return queryset
-        elif len(recipe) > 0 and len(cuisines) > 0 and len(meal_types) == 0:
+        elif len(recipe) > 0 and len(cuisines) > 0 and len(meal_types) == 0 and len(ingredients) == 0:
             queryset = Recipe.objects.filter(
                 Q(name_recipe__icontains=recipe) &
                 Q(cuisine__in=cuisines)
             )
             return queryset
-        elif len(recipe) == 0 and len(cuisines) == 0 and len(meal_types) > 0:
+        elif len(recipe) > 0 and len(cuisines) == 0 and len(meal_types) > 0 and len(ingredients) == 0:
+            queryset = Recipe.objects.filter(
+                Q(name_recipe__icontains=recipe) &
+                Q(meal_type__in=meal_types)
+            )
+            return queryset
+        elif len(recipe) > 0 and len(cuisines) == 0 and len(meal_types) == 0 and len(ingredients) > 0:
+            queryset = Recipe.objects.filter(
+                Q(name_recipe__icontains=recipe) &
+                Q(connections__id_ingr__group_name_ingr__in=ingredients)
+            )
+            return queryset
+        elif len(recipe) == 0 and len(cuisines) > 0 and len(meal_types) > 0 and len(ingredients) == 0:
+            queryset = Recipe.objects.filter(
+                Q(cuisine__in=cuisines) &
+                Q(meal_type__in=meal_types)
+            )
+            return queryset
+        elif len(recipe) == 0 and len(cuisines) > 0 and len(meal_types) == 0 and len(ingredients) > 0:
+            queryset = Recipe.objects.filter(
+                Q(cuisine__in=cuisines) &
+                Q(connections__id_ingr__group_name_ingr__in=ingredients)
+            )
+            return queryset
+        elif len(recipe) == 0 and len(cuisines) == 0 and len(meal_types) > 0 and len(ingredients) > 0:
+            queryset = Recipe.objects.filter(
+                Q(meal_type__in=meal_types) &
+                Q(connections__id_ingr__group_name_ingr__in=ingredients)
+            )
+            return queryset
+        elif len(recipe) == 0 and len(cuisines) == 0 and len(meal_types) > 0 and len(ingredients) == 0:
             queryset = Recipe.objects.filter(meal_type__in=meal_types)
             return queryset
-        elif len(recipe) == 0 and len(cuisines) > 0 and len(meal_types) == 0:
+        elif len(recipe) == 0 and len(cuisines) > 0 and len(meal_types) == 0 and len(ingredients) == 0:
             queryset = Recipe.objects.filter(cuisine__in=cuisines)
             return queryset
-        elif len(recipe) > 0 and len(cuisines) == 0 and len(meal_types) == 0:
+        elif len(recipe) > 0 and len(cuisines) == 0 and len(meal_types) == 0 and len(ingredients) == 0:
             queryset = Recipe.objects.filter(name_recipe__icontains=recipe)
+            return queryset
+        elif len(recipe) == 0 and len(cuisines) == 0 and len(meal_types) == 0 and len(ingredients) > 0:
+            queryset = Recipe.objects.filter(connections__id_ingr__group_name_ingr__in=ingredients)
             return queryset
         else:
             queryset = Recipe.objects.all()
