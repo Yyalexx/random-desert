e4ae123b9f4639089aa7e087f12fdffe7729c5ea
semyonsorokin <simonlen.gba@gmail.com>
2023-04-06 16:49:10 +0300

    Comments added

--- compose/django_app/django_app/settings.py
+++ compose/django_app/django_app/settings.py
@@ -130,14 +130,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # for NGNIX
 
+# ----- uncomment for development -----
 # STATICFILES_DIRS = [
 #     BASE_DIR / "static"
 # ]
+# ----- uncomment for development -----
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(STATIC_ROOT, "media")
+
+MEDIA_URL = 'media/'  # for NGNIX
+MEDIA_ROOT = os.path.join(STATIC_ROOT, "media")  # for NGNIX
 
 
 # Default primary key field type
