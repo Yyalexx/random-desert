8f79a4fae5c8ee8817489781676ab3744540e29c
semyonsorokin <simonlen.gba@gmail.com>
2023-04-06 16:45:49 +0300

    Setted up for production

--- compose/django_app/django_app/settings.py
+++ compose/django_app/django_app/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['213.193.7.201', 'randomdessert.ddns.net', '127.0.0.1',]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -130,10 +130,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static"
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static"
+# ]
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(STATIC_ROOT, "media")
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
